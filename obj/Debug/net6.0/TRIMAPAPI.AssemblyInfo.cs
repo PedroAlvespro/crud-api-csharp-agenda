7233af5cffb7b3adba7fd3edef0a5c486b9041de
Pedro Alves <pedrosabiodos6caminhos@gmail.com>
2024-12-23 08:50:15 -0300

    refactor: mais configuracoes jwt

--- obj/Debug/net6.0/TRIMAPAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/TRIMAPAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRIMAPAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273ad2721cb67ff1bf7669e0283f181af7246c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d10156297a9095d7d80cc54bb9be7e0ac84e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRIMAPAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRIMAPAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
